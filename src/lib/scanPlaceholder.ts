6da64d68802ae8eae79aa34dbe257909a6b92d0c
xxSagnikxx <sagnik2006majumdar@gmail.com>
2026-04-21 02:00:59 +0530

    Sync local deletions

--- src/lib/scanPlaceholder.ts
+++
@@ -1,7 +0,0 @@
-import { toast } from "sonner";
-
-export function runScanPlaceholder(): void {
-  toast.message("Scan queued", {
-    description: "Connect backend services to execute integrity scans.",
-  });
-}
